c7b2028e2b9f0e680a6888bfb4a84eed2a6bc980
code-panic <32615067+phs1234@users.noreply.github.com>
2022-12-12 19:54:56 +0900

    first commit

---
+++ Arduino/CloudLED_Hardware.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+
+#define TX 2
+#define RX 3
+
+SoftwareSerial bluetoothSerial(TX, RX);   // 블루투스 객체 선언
+String message;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  bluetoothSerial.begin(9600);
+}
+
+void loop() {
+  if (bluetoothSerial.available()) {
+      char c = bluetoothSerial.read();
+
+      if (c == '@') {
+        Serial.println(message);
+        message = "";
+      } else {
+        message += c; 
+      }
+  }
+}
